8506a7aefa89accaaba3ffa43069e7d8d428d6f7
gereltod <gereltod.s@gmail.com>
2017-03-14 13:37:02 +0800

    password hashing bcrypt

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id:10
-};
+var password = '123asdf!';
 
-var token = jwt.sign(data, '123asdf');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash)=>{
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token, '123asdf');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$NpRYjlfQT3MC6fgc7EkS2.pIbtmrQR6Bd1w9zU/9vbh.E0Mamjj8G';
+
+bcrypt.compare(password, hashedPassword, (err, res) =>{
+    console.log(res);
+});
+
+// var data = {
+//     id:10
+// };
+
+// var token = jwt.sign(data, '123asdf');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123asdf');
+// console.log('decoded', decoded);
 
 // var message = 'I am user number 3';
 
